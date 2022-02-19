61794163e26cc02d947bc94ff1f7c6d960441628
Christopher Owens <cowensdev@gmail.com>
2022-02-19 08:46:15 -0500

    Structure controllers and models to handle appropriate responsibilities

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -1,9 +1,9 @@
-const { launches } = require('../../models/launches.model');
+const launchesModel = require('../../models/launches.model');
 
-function getAllLaunches(req, res) {
-    return res.status(200).json(Array.from(launches.values()));
+function httpGetAllLaunches(req, res) {
+    return res.status(200).json(launchesModel.getAllLaunches());
 }
 
 module.exports = {
-    getAllLaunches,
+    httpGetAllLaunches,
 };
